b2a8471c9d1d3852f4a07b39aa4667e2980d6362
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2025-01-26 13:26:10 +0530

    get response function

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE-API-KEY"))
 
+
+# ------------------FUNCTIONS------------------
+
+def get_gemini_response(input_text, pdf_content, prompt):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content([input_text, pdf_content[0], prompt])
+    return response.text
+
+
 # ------------------UI------------------------
 
 st.text_input("Enter the job description...")
