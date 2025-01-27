2fd2e12078bcba2aa9f5e64c403d3cf8661f9e01
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2025-01-27 21:26:23 +0530

    updating function

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ genai.configure(api_key=os.getenv("GOOGLE-API-KEY"))
 
 # ------------------FUNCTIONS------------------
 
-# def get_gemini_response(input_text, pdf_content, prompt):
-#     model = genai.GenerativeModel("gemini-1.5-flash")
-#     response = model.generate_content([input_text, pdf_content[0], prompt])
-#     return response.text
+def get_gemini_response(input_text, pdf_content, prompt):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content([input_text, pdf_content[0], prompt])
+    return response.text
 
 
 # ------------------UI------------------------
