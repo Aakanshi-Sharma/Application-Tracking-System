cf053db83fc10acbb95371838154ea995b8da31e
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2025-02-04 15:49:25 +0530

    pdf_setup

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import base64
+import io
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -16,6 +18,26 @@ def get_gemini_response(input_text, pdf_content, prompt):
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content([input_text, pdf_content[0], prompt])
     return response.text
+
+
+def input_pdf_setup(uploaded_file):
+    if uploaded_file is not None:
+        images = pdf2image.convert_from_bytes(uploaded_file.read())
+        first_page = images[0]
+
+        img_byte_arr = io.BytesIO()
+        first_page.save(img_byte_arr, format="JPEG")
+        img_byte_arr = img_byte_arr.getvalue()
+
+        pdf_parts = [
+            {
+                "mime_type": "image/jpeg",
+                "data": base64.b64encode(img_byte_arr).decode()
+            }
+        ]
+        return pdf_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
 
 
 # ------------------UI------------------------
