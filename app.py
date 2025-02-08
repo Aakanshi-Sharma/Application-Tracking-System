31e6dc8bfc1f6030ec771a87615ab46f6f7814a3
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2025-02-08 13:57:32 +0530

    partially created

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_gemini_response(input_text, pdf_content, prompt):
 
 def input_pdf_setup(uploaded_file):
     if uploaded_file is not None:
-        images = pdf2image.convert_from_bytes(uploaded_file.read())
+        images = pdf2image.convert_from_bytes(uploaded_file.read(), poppler_path=r'C:\Program Files\poppler\Library\bin')
         first_page = images[0]
 
         img_byte_arr = io.BytesIO()
@@ -52,14 +52,14 @@ submit2 = st.button("How can I Improvise my skills")
 submit3 = st.button("Percentage match")
 
 input_prompt1 = """
-You are an experienced HR with Tech Experience in the field of data science, full stack development,
+You are an experienced HR with Tech Experience in the field of any one job from  data science, full stack development,
 big Data engineering, DEVOPS, Data analyst, your task is to review the provided resume against the job description.
 Please share your professional evaluation on whether the candidate's profile align with the role.
 Highlight the strengths and weaknesses of the applicant in relation to that specified job requirements. 
 """
 
 # input_prompt2 = """
-# You are an Technical HR with expertise in data science, full stack development,
+# You are an Technical HR with expertise in  any one job from  data science, full stack development,
 # big Data engineering, DEVOPS, Data analyst,
 #  your role is to scrutinize the resume in light of the job description provided.
 #  Share your insights on the candidate's suitability for the role from an HR perspective,
@@ -67,11 +67,26 @@ Highlight the strengths and weaknesses of the applicant in relation to that spec
 # """
 
 input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science, full stack development,
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of  any one job from  data science, full stack development,
 big Data engineering, DEVOPS, Data analyst, and deep ATS functionality,
 Your task is to evaluate the resume against the provided job description, give me the percentage of match if the resume matches 
 job description. First the output should come as percentage and then keywords missing and last final thoughts. 
 """
 
 if submit1:
-    pass
+    if uploaded_file is not None and input_text !="":
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_text, pdf_content, input_prompt1)
+        st.write("The response : ", response)
+
+    else:
+        st.write("Uploaded file and job description can't be empty!")
+
+elif submit3:
+    if uploaded_file is not None and input_text !="":
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_text, pdf_content, input_prompt3)
+        st.write("The response : ", response)
+
+    else:
+        st.write("Uploaded file and job description can't be empty!")
