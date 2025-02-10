fce0c05a21c8b06f328d1b8ca3cc864553df0e71
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2025-02-10 22:56:39 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import streamlit as st
-import google.generativeai as genai
 import os
+
 import PyPDF2 as pdf
+import google.generativeai as genai
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,4 +16,34 @@ def get_genai_response(input_text):
     return response.text
 
 
+def input_pdf_text(uploaded_file):
+    reader = pdf.PdfReader(uploaded_file)
+    text = ""
+    for page in reader(len(reader.pages)):
+        page = reader.pages[page]
+        text += str(page.extract_text())
+        text += " "
+    return text
 
+
+input_prompt = """
+Hey Act Like a skilled or very experience ATS(Application Tracking System)
+with a deep understanding of tech field, software engineering, data science, data analyst
+and big data engineer. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide
+best assistance for improving the resumes. Assign the percentage Matching based
+on JD and missing keywords with high accuracy.
+resume: {text}
+description: {jd}
+I want the response in one single string having the structure
+{{"JD Match": "%", "MissingKeywords:[]", "Profile Summary":""}}
+"""
+
+# ----------------------UI---------------------
+
+
+st.title("ATS")
+st.text("Improve your Resume ATS")
+jd = st.text_area("Paste the job Description")
+uploaded_files = st.file_uploader("Upload your resume", type="pdf", help="Please upload resume in pdf format")
+submit_button = st.button("Submit")
